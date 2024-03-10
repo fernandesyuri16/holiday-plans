c90e9992c5c40ff4c7e26b23ee66071eef8caa32
Yuri Fernandes <yuricaparelliofc@gmail.com>
2024-03-10 19:11:33 -0300

    creating routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\HolidayPlanController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware("auth:sanctum")->get("/user", function (Request $request) {
-    return $request->user();
+Route::post('/users', [UserController::class, 'store'])->name('user.store');
+
+Route::middleware("auth:sanctum")->group(function () {
+    Route::apiResource('users', UserController::class)->except('store');
+    Route::apiResource('holiday-plans', HolidayPlanController::class);
 });
