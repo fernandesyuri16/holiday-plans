d798d33fb0cbdd3e9b47ce32c546a15d144e0436
Yuri Fernandes <yuricaparelliofc@gmail.com>
2024-03-14 22:58:28 -0300

    creating documentation

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -7,6 +7,32 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 /**
+ * @OA\Info(
+ *      version="0.0.1",
+ *      title="Holiday Plans - Docs",
+ *      description="API documentation for the <b>Holiday Plans</b> project",
+ *      @OA\Contact(
+ *          email="yuricaparelliofc@gmail.com"
+ *      )
+ * )
+ *
+ * @OA\Server(
+ *      url="http://localhost:8001/api",
+ *      description="API url"
+ * )
+ *
+ * @OA\SecurityScheme(
+ *      type="apiKey",
+ *      description="Insert the token in the format: <b>Bearer {token}</b>",
+ *      in="header",
+ *      name="Authorization",
+ * 		scheme="sanctum",
+ * 		securityScheme="sanctum"
+ * )
+ *
+ */
+
+/**
  * Class Controller
  *
  * This is the base controller class that all other controllers extend from.
